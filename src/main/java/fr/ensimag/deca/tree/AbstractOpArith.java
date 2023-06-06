4891c972dc4fc834749f9faea16ecb33c4f304cf
Anas El Ahmar <anas.el-ahmar@grenoble-inp.org>
2023-06-06 16:40:06 +0200

    [ADD] context ok for arithmetic

--- src/main/java/fr/ensimag/deca/tree/AbstractOpArith.java
+++ src/main/java/fr/ensimag/deca/tree/AbstractOpArith.java
@@ -21,6 +21,44 @@ public abstract class AbstractOpArith extends AbstractBinaryExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        throw new UnsupportedOperationException("not yet implemented");
+
+        Type t1;
+        Type t2;
+
+        try{
+            t1 = this.getLeftOperand().verifyExpr(compiler, localEnv, currentClass);
+            t2 = this.getRightOperand().verifyExpr(compiler, localEnv, currentClass);
+        } catch (ContextualError e) {
+            throw e;
+        }
+
+        //if one of the operands is not a number: problem !
+        
+        if (!t1.isInt()&&!t1.isFloat()) {
+            throw new ContextualError("left operand is not an int nor a float",this.getLocation());
+
+        } else if (!t2.isInt()&&!t2.isFloat()){
+            throw new ContextualError("right operand is not an int nor a float",this.getLocation());
+        
+        // both operands are valid numbers
+        } else {
+            // operands have different types 
+            if (t1.isFloat() && t2.isInt()){
+                // convert int to float
+                this.setRightOperand(this.getRightOperand().verifyRValue(compiler, localEnv, currentClass,t1));
+                // we set the type of the expression to float (t1)
+                this.setType(t1);
+                return t1;
+            } else if (t1.isInt() && t2.isFloat()) {
+                // convert int to float
+                this.setLeftOperand(this.getLeftOperand().verifyRValue(compiler, localEnv, currentClass, t2));
+                // we set the type of the expression to float (t2)
+                this.setType(t2);
+                return t2;
+            }
+        }
+        // here it means operands have the same type, we can set the type of the expression to t1 for example
+        this.setType(t1);
+        return t1;
     }
 }
