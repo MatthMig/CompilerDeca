4d75dd5a81eb788478f64d4a838041fbe9fa820e
Jules Vandeputte <jules.vandeputte@grenoble-inp.org>
2023-06-06 13:15:27 +0000

    [fix]#1#2 Correction bugs Lower et Float

--- src/main/java/fr/ensimag/deca/tree/LowerOrEqual.java
+++ src/main/java/fr/ensimag/deca/tree/LowerOrEqual.java
@@ -7,6 +7,7 @@ import fr.ensimag.ima.pseudocode.GPRegister;
 import fr.ensimag.ima.pseudocode.instructions.BLE;
 import fr.ensimag.ima.pseudocode.instructions.BGT;
 import fr.ensimag.ima.pseudocode.instructions.CMP;
+import fr.ensimag.ima.pseudocode.instructions.FLOAT;
 import fr.ensimag.ima.pseudocode.instructions.LOAD;
 
 /**
@@ -27,16 +28,44 @@ public class LowerOrEqual extends AbstractOpIneq {
 
     @Override
     public void codeGenCondition(DecacCompiler compiler, Boolean neg, Label label) {
-        ExpDefinition varDef = compiler.getVar(((Identifier)this.getLeftOperand()).getName());
-        compiler.addInstruction(new LOAD(varDef.getOperand(), GPRegister.getR(2)));
-
-        if (this.getRightOperand().getClass() == IntLiteral.class) {
-            compiler.addInstruction(new CMP( ((IntLiteral)this.getRightOperand()).getValue() , GPRegister.getR(2)));
-        } else {
-            ExpDefinition rightVar = compiler.getVar(((Identifier)this.getRightOperand()).getName());
-            compiler.addInstruction(new LOAD(rightVar.getOperand(), GPRegister.getR(1)));
-            compiler.addInstruction(new CMP(GPRegister.getR(1), GPRegister.getR(2)));
+        // Check Left Value's Type
+        if (this.getLeftOperand().getClass() == Identifier.class) {
+            // Load a variable content to R2
+            ExpDefinition leftVar = compiler.getVar(((Identifier)this.getLeftOperand()).getName());
+            compiler.addInstruction(new LOAD(leftVar.getOperand(), GPRegister.getR(2)));
+        } else if (this.getLeftOperand().getType().isInt()) {
+            // Load an Int to R2
+            compiler.addInstruction(new LOAD(((IntLiteral)this.getLeftOperand()).getValue(), GPRegister.getR(2)));
+        } else if (this.getLeftOperand().getType().isFloat()) {
+            // Load a Float to R2
+            compiler.addInstruction(new LOAD(((FloatLiteral)this.getLeftOperand()).getValue(), GPRegister.getR(2)));
         }
+
+        // Check Right Value's Type
+        if (this.getRightOperand().getClass() == Identifier.class) {
+            // Load a variable content to R3
+            ExpDefinition rightVar = compiler.getVar(((Identifier)this.getRightOperand()).getName());
+            compiler.addInstruction(new LOAD(rightVar.getOperand(), GPRegister.getR(3)));
+        } else if (this.getRightOperand().getType().isInt()) {
+            // Load an Int to R3
+            compiler.addInstruction(new LOAD(((IntLiteral)this.getRightOperand()).getValue(), GPRegister.getR(3)));
+        } else if (this.getRightOperand().getType().isFloat()) {
+            // Load a Float to R3
+            compiler.addInstruction(new LOAD(((FloatLiteral)this.getRightOperand()).getValue(), GPRegister.getR(3)));
+        }
+
+        // If we have a float, we need to cast the one that is not
+        if (this.getLeftOperand().getType().isFloat() ^ this.getRightOperand().getType().isFloat()) {
+            if (this.getLeftOperand().getType().isFloat()) {
+                compiler.addInstruction(new FLOAT(GPRegister.getR(3), GPRegister.getR(3)));
+            } else {
+                compiler.addInstruction(new FLOAT(GPRegister.getR(2), GPRegister.getR(2)));
+            }
+        }
+        
+        // then compare
+        compiler.addInstruction(new CMP(GPRegister.getR(3), GPRegister.getR(2)));
+
         if (!neg) {
             compiler.addInstruction(new BLE(label));
         } else {
