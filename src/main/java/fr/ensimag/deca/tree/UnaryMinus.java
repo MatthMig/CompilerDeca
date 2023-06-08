5e646173e685f76ec915462644136d0a816905c8
telliere <emilien.tellier@grenoble-inp.org>
2023-06-09 01:22:39 +0200

    [feature]#6 Implémentation de gencode et autres méthodes diverses pour Not, Minus et les expressions unaires plus globalement

--- src/main/java/fr/ensimag/deca/tree/UnaryMinus.java
+++ src/main/java/fr/ensimag/deca/tree/UnaryMinus.java
@@ -1,6 +1,11 @@
 package fr.ensimag.deca.tree;
 
 import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.GPRegister;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+import fr.ensimag.ima.pseudocode.instructions.OPP;
+import fr.ensimag.ima.pseudocode.instructions.SUB;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
@@ -19,7 +24,7 @@ public class UnaryMinus extends AbstractUnaryExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        
+
         Type t;
 
         try {
@@ -27,7 +32,7 @@ public class UnaryMinus extends AbstractUnaryExpr {
         } catch (ContextualError e) {
             throw e;
         }
-        
+
         // if operand is not a number: problem !
         if (!t.isInt() && !t.isFloat()) {
             throw new ContextualError("operand after minus is not a number",this.getOperand().getLocation());
@@ -38,6 +43,13 @@ public class UnaryMinus extends AbstractUnaryExpr {
         return t;
     }
 
+    @Override
+    protected void codeGenExp(DecacCompiler compiler, int n) {
+
+        // Generate code for operand
+        this.getOperand().codeGenExp(compiler, n);
+        compiler.addInstruction(new OPP(GPRegister.getR(n),GPRegister.getR(n)));
+    }
 
     @Override
     protected String getOperatorName() {
