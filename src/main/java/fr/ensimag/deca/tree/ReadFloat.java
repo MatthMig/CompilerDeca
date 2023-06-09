5c329638e99c76a3d5496f9d1ec4345bf874fe67
Anas El Ahmar <anas.el-ahmar@grenoble-inp.org>
2023-06-09 10:36:53 +0200

    [UPDATE] small fix for readInt + readFloat

--- src/main/java/fr/ensimag/deca/tree/ReadFloat.java
+++ src/main/java/fr/ensimag/deca/tree/ReadFloat.java
@@ -6,8 +6,12 @@ import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+import fr.ensimag.ima.pseudocode.instructions.RFLOAT;
+import fr.ensimag.ima.pseudocode.instructions.BOV;
+import fr.ensimag.ima.pseudocode.Register;
+import fr.ensimag.ima.pseudocode.Label;
 import java.io.PrintStream;
-
 /**
  *
  * @author gl03
@@ -38,4 +42,11 @@ public class ReadFloat extends AbstractReadExpr {
         // leaf node => nothing to do
     }
 
+    @Override
+    protected void codeGenExp(DecacCompiler compiler, int n) {
+        compiler.addInstruction(new RFLOAT());// this loads the input in R1
+        compiler.addInstruction(new BOV(new Label("io_error")));
+        // load the input to Rn
+        compiler.addInstruction(new LOAD(Register.R1, Register.getR(n)));
+    }
 }
